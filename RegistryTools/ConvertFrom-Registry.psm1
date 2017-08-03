fb51a0a9023ee5e7b40038e636e521fb98b2f9a8
Brent <brent.g.wright@gmail.com>
2017-08-03 07:33:30 -0400

    Initial upload of RegistryTools module.
    ConvertFrom-Registry and Set-RegistryObject functions are working well at this point. Some Pester tests have been created for each.

---
+++ RegistryTools/ConvertFrom-Registry.psm1
@@ -0,0 +1,177 @@
+<#
+.Synopsis
+   A function to retrieve properties from registry key(s), and convert it and all subkeys into an object.
+.DESCRIPTION
+   The path given will be the start of the search, and it will recursively search subkeys for properties to return.
+
+   Default number of levels to recursively search is 2 levels. Use the -Levels parameter to search deeper.
+   Default number of subkeys to return are 10 subkeys. Use the -MaxSubKeys parameter to return more subkeys.
+
+   Note: Be aware that this returns properties of subkeys, if a subkey contains no properties it may not be seen in the results.
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   The component this cmdlet belongs to
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+
+function ConvertFrom-Registry {
+    [cmdletbinding()]
+    param(
+    # Path to the base key in registry.
+    [Parameter(Mandatory=$true,
+               Position=0,
+               ValueFromPipeline=$true,
+               ValueFromPipelineByPropertyName=$true)]
+    [Alias("PSPath")]
+    $Path,
+    # Number of levels to retrieve properties from. Default = 3
+    [int]
+    $Levels = 2,
+    # Maximum number of subkeys to explore.
+    [int]
+    $MaxSubKeys = 10
+    )
+
+    # Retrieve the item from the path given.
+    $regKey = Get-Item $Path -ErrorAction Stop
+
+    # Check that path leads to a valid item.
+    if ($regKey -is [Microsoft.Win32.RegistryKey]) {
+        
+        # Retrieves values in the key, and recurses through some level of subkeys for values.
+        $results = RegistryObjectHelper -Path $regKey -Level $Levels -MaxSubKeys $MaxSubKeys
+
+    # Otherwise, exit with the error thrown by Get-Item.
+    } else {
+        Write-Error -Category InvalidArgument -Message "Path does not point to a registry key."
+        return
+    }
+
+    #$dupValueProps = $results | foreach {
+    #    $_ | Get-Member | Where-Object { $_.membertype -eq "NoteProperty" } } |
+    #    Group-Object -Property Name | Where-Object {$_.count -gt 1} | select -ExpandProperty Name
+        
+
+    $outputParams = [ordered]@{
+        "PSTypeName" = "RegistryTools.RegistryObject";
+        "PSPath" = $regKey.PSPath;
+        "PSDrive" = $regKey.PSDrive;
+        "PSParentPath" = $regKey.PSParentPath;
+        "PSChildName" = $regKey.PSChildName;
+    }
+
+    foreach ($key in $results) {
+        
+        # Get a list of properties from each key, excluding the common properties.
+        $propList = $key | Get-Member | 
+            Where-Object { $_.MemberType -eq "NoteProperty"} |
+            Select-Object -ExpandProperty Name | 
+            Where-Object { @("PSPath","PSChildName", "PSParentPath","PSProvider") -notcontains $_}
+        
+        # Add the values to the output.
+        foreach ($prop in $propList) {
+            
+            <#
+            # Get the common properties for the key value.
+            $propParams = @{
+                'PSPath' = $key.PSPath;
+                'PSChildName' = $key.PSChildName;
+                'PSParentPath' = $key.PSParentPath;
+                'PSProvider' = $key.PSProvider;
+            }
+
+            $propName  = ""
+
+            # Check if value has already been stored from previous key.
+            if ($dupValueProps -contains $prop) {
+
+                $propName = "$($key.PSChildname)\$prop"
+                
+                # Add the name of the parent key to keep property names unique.
+                $propParams += @{$propName = $key.$prop;}
+
+            } else {
+                
+                $propName = "$prop"
+                
+                # Add the name of the property and value.
+                $propParams += @{$propName = $key.$prop;}
+                
+            }
+
+            # Add the value to the output hashtable.
+            $outputParams += @{$propName = $(new-object psobject -Property $propParams) }
+
+            #>
+
+            # Removes the ending "\" in the path, if given in the parameters.
+            $regKeyPath = $regKey.pspath -replace "\\$",""
+
+            # Gets the relative path of the value, using the given Path as root.
+            $propKey = Join-path $key.PSPath.substring($regKeyPath.length+1) $prop
+
+            $propValue = $key.$prop
+
+            $outputParams += @{$propKey = $propValue;}
+
+        }
+
+    }
+
+    # Convert the output to an object, give it a typename, then return it.
+    #$outputParams = new-object psobject -Property $outputParams
+    #$outputParams.psobject.typenames.insert(0,"RegistryTools.RegistryObject")
+    #$outputParams | Write-Output
+
+    new-object psobject -Property $outputParams | Write-Output
+
+}
+
+function RegistryObjectHelper {
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true)]
+        [Microsoft.Win32.RegistryKey]
+        $Path,
+        [int]
+        $Level,
+        [int]
+        $MaxSubKeys
+
+    )
+
+    Begin {   }
+    Process {
+
+        Write-Verbose "Getting values from $($Path.pspath)"
+        Get-ItemProperty -Path $Path.pspath
+
+        if ($Path.SubKeyCount -gt 0 -and $Level -gt 0) {
+
+            if ($Path.subkeycount -gt $MaxSubKeys) {
+                Write-Warning "There are more than $MaxSubKeys subkeys under $(Split-Path $Path.pspath -NoQualifier). To view more, update the -MaxSubKeys parameter. Total subkey count is $($Path.subkeycount)."
+            }
+
+            $Path.getSubKeyNames() | 
+                Select-Object -First $MaxSubKeys |
+                ForEach-Object { Get-Item -path (Join-Path $Path.pspath $_); Write-Verbose "Recursing into $_" } |
+                RegistryObjectHelper -Level ($Level - 1) -MaxSubKeys $MaxSubKeys
+
+        }
+    }
+
+    End {   }
+}
---
+++ RegistryTools/Set-RegistryObject.psm1
@@ -0,0 +1,244 @@
+
+
+<#
+.Synopsis
+   Takes an object, retrieved using "ConvertFrom-Registry" and has been modified, to update the registry with the changes.
+.DESCRIPTION
+   Takes a [RegistryTools.RegistryObject] and uses each non-common property to check if modifications were made, and to update the registry with the changes.
+
+   Common Properties:
+   - PSPath
+   - PSDrive
+   - PSParentPath
+   - PSChildName
+
+   Each non-common property will contain the relative path to the registry value.
+   The value of the non-common property will be the data value for the registry value.
+
+   Registry Structure overview:
+   . Registry Key
+   . . Registry Key
+   . . + Registry Value = Data Value
+
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   General notes
+.COMPONENT
+   The component this cmdlet belongs to
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+
+function Set-RegistryObject {
+    [cmdletbinding(SupportsShouldProcess=$true,ConfirmImpact="High")]
+    Param(
+    # A [RegistryTool.RegistryObject] object that has been modified, and will be used to update the registry.
+    [Parameter(Mandatory=$true,
+               ValueFromPipeline=$true)]
+    $InputObject,
+    # Path for a backup file to be created. This will contain a script that restores the updated registry values.
+    [Parameter(Mandatory=$true)]
+    $BackupFile,
+    # Forces the registry to be updated with the InputObject values. Also can force the backup file to be created.
+    [switch]
+    $Force
+    )
+    
+    Begin {
+        
+        # Common Properties
+        $commonProp = @("PSPath", "PSDrive", "PSParentPath", "PSChildName")
+
+        $backupFileHeader = @"
+### Registry Restore Script ###
+
+# Username: $env:USERDNSDOMAIN\$env:USERNAME
+# Created: $(Get-Date -Format "MMMM dd yyyy @ hh:mm:ss tt")
+
+# Registry Root Path: $($InputObject.PSPath)
+
+# Instructions for use:
+# Execute this script to restore the previous registry settings.
+
+"@
+
+        # Check if backup file exists, otherwise create it. Restore commands will be appended to files that already exist.
+        if (Test-Path -Path $BackupFile -PathType Leaf) {
+            Write-Verbose "Backup file exists, restore commands will be appended."
+
+            $backupFileCreated = $false
+        } else {
+            
+            # Check if path is a directory. Directory path ends with a slash. All else is assumed a file.
+            if ($([System.IO.FileInfo]$BackupFile).Name -eq "") {
+                
+                # Add a filename to the backup path.
+                $BackupFileName = "RegistryRestore-$(get-date -Format 'yyyyMMdd_HH.mm.ss').ps1"
+
+                Write-Verbose "BackupFile given is a directory, adding $BackupFileName"
+
+                # Add the filename to the backup file path.
+                $backupFile = Join-Path $BackupFile $BackupFileName
+            }
+
+            New-Item -Path $BackupFile -ItemType File -Force:$Force | Out-Null
+
+            Write-Verbose "Backup file created, restore commands will be added."
+
+            $backupFileCreated = $true
+        }
+
+        # Flag to remove backup file if not used.
+        $backupFileUpdated = $false
+
+    }
+
+    End {
+        
+        # If backup file was created and not used, remove it.
+        if ($backupFileUpdated -eq $false -and $backupFileCreated -eq $true) {
+
+            Write-Verbose "Registry not updated, removing created backup file."
+            Remove-Item -Path $BackupFile -Force
+        }
+    }
+
+    Process {
+        
+        if ($InputObject.psobject.typenames[0] -ne "RegistryTools.RegistryObject") {
+            Write-Error -Category InvalidData -Message "InputObject is expected to be a [RegistryTools.RegistryObject] from ConvertFrom-Registry."
+        }
+        
+        # Get a list of registry properties in the object.
+        $propertyList = $InputObject | 
+            Get-Member -MemberType NoteProperty | 
+            Where-Object {$commonProp -notcontains $_.Name} | 
+            Select-Object -ExpandProperty Name
+
+        # Process each non-common property in the object.
+        $results = $propertyList | CompareRegValueHelper -RegistryObject $InputObject -Force:$Force
+
+        if ($results.RestoreCommands.count -gt 0) {
+            
+            # Set flag to preserve the backup file.
+            $backupFileUpdated = $true
+
+            # Add the restore commands to the backup file.
+            $results.RestoreCommands | Out-File -FilePath $backupFile -Encoding ascii -Append
+        }
+
+    } # Process
+
+}
+
+Function CompareRegValueHelper {
+    [cmdletbinding()]
+    Param(
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true)]
+        $RegObjProperty,
+        $RegistryObject,
+        [switch]
+        $Force
+    )
+
+    Begin {
+
+        # Prepare to confirm each registry update.
+        $yesToAll = $Force # Sets initial value from -Force
+        $noToAll = $false
+
+        # Prepare to confirm after permission denied exception.
+        $permYesToAll = $Force # Sets initial value from -Force
+        $permNoToAll = $false
+
+        $output = New-Object psobject -Property @{'UpdatedKeys'=@{};'RestoreCommands'=@();}
+
+        $continue = $true
+
+    }
+
+    Process {
+
+        # Continue to process updates, unless permission denied and user aborts.
+        if ($continue) {
+
+            # Join the PSPath and the relative path to the property together. Leaving out the registry property name.
+            $regPath = Join-Path $RegistryObject.pspath $(Split-Path $RegObjProperty -Parent)
+
+            # Extract the registry property name from the object property.
+            $propertyName = Split-Path $RegObjProperty -Leaf
+            
+            # Get the current value of the registry property.
+            $currentValue = Get-ItemProperty -Path $regPath -Name $propertyName | Select-Object -ExpandProperty $propertyName
+
+            if (@(Compare-Object -ReferenceObject $currentValue -DifferenceObject $RegistryObject.$RegObjProperty).count -eq 0) {
+                Write-Verbose "$RegObjProperty property is the same, no update necessary."
+            } else {
+
+                Write-Verbose "$RegObjProperty property is different, and will be updated."
+
+                $confirmMessage = "Update $regPath`nOldValue: $currentValue`nNew Value: $($RegistryObject.$RegObjProperty)"
+
+                # Confirm updating the registry key.
+                if ($PSCmdlet.ShouldContinue("Set-ItemProperty",$confirmMessage,[ref]$yesToAll,[ref]$noToAll)) {
+                    
+                    # Add the restore line to the backup file.
+                    $output.RestoreCommands += "Set-ItemProperty -Path $regPath -Name $propertyName -Value `"$currentValue`"`n`n"
+
+                    # Clear the error catcher.
+                    $registryErrors = $null
+
+                    Set-ItemProperty -Path $regPath -Name $propertyName -Value $RegistryObject.$RegObjProperty -Force:$Force -ErrorVariable registryErrors
+
+                    # Add the registry key update to the output.
+                    $output.UpdatedKeys += @{$RegObjProperty = $($RegistryObject.$RegObjProperty)}
+
+                    # Check if errors occured during registry update.
+                    if ($registryErrors.count -gt 0) {
+
+                        # Process each error.
+                        foreach ($err in $registryErrors) {
+                            
+                            # Perform the checks on whether to continue or stop attempting to update
+                            $permissionDenied = $err.CategoryInfo.Category -eq "PermissionDenied"
+                            $permDeniedshouldContinue = $PSCmdlet.ShouldContinue("Set-ItemProperty","Unable to update registry key(PermissionDenied), continue updating other registry keys?",[ref]$permYesToAll,[ref]$permNoToAll)
+                            
+                            # Check if error is "PermissionDenied" and the user wants to continue.
+                            if ($permissionDenied -and $permDeniedshouldContinue) {
+                                
+                                Write-Verbose "Continuing to update registry keys after receiving `"PermissionDenied`" error."
+                            
+                            } else {
+                                
+
+                                $continue = $false
+
+                                return # exit the function
+                            } # If: ShouldContinue - PermissionDenied
+
+                        } # Foreach: registryErrors
+
+                    } # If: RegistryError > 0
+
+                } # If: ShouldContinue - Update Registry
+
+            } # Else: Values are 
+
+        } # If: Continue
+    } # Process
+
+    End {   
+
+        Write-Output $output
+    }
+}
